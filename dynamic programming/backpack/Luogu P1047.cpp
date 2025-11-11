06cf89ab553fff0094593304d8aaeee4e7d948b4
SHOJYS <74080161+SHOJYS@users.noreply.github.com>
2025-11-11 08:51:17 +0800

    Add solution for backpack problem in Luogu P1047

---
+++ dynamic programming/backpack/Luogu P1047.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<algorithm>
+typedef std::pair<int,int> PII;
+#define P(A) A=-~A
+typedef long long LL;
+#define NUMBER1 20000
+#define NUMBER2 400
+int n,f[NUMBER1+5],v,a[NUMBER2+5];
+signed main(){
+	std::cin.tie(nullptr)->std::ios::sync_with_stdio(false);
+	std::cout.tie(nullptr);
+	std::cin>>v>>n;
+	for(int i=1;i<=n;P(i))std::cin>>a[i];
+	for(int i=1;i<=n;P(i))
+		for(int j=v;j>=a[i];--j)f[j]=std::max(f[j],f[j-a[i]]+a[i]);
+	std::cout<<v-f[v];
+	return 0;
+}
